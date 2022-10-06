b33bd754821e9df9c7489fd0882bb0b947319ed2
muhammadi <mdbarcha5@gmail.com>
2022-10-06 04:09:15 -0700

    array methods

---
+++ app.js
@@ -0,0 +1,25 @@
+// array methods
+
+
+// pop
+
+let array = ["yaqub","imram","wajahat"]
+console.log(array)
+
+array.pop()
+console.log(array)
+
+// push
+array.push("muhammadi")
+console.log(array)
+
+// shift
+
+array.shift()
+console.log(array)
+
+// unshift
+
+array.unshift("ali",22)
+console.log(array)
+
